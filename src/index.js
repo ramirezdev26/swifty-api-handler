e9070e119d214c0e8e21d862a2955f96afed3c0b
JuanD_LG <Juan.Lemus@jala.university>
2025-10-09 10:53:16 -0500

    feat(auth): add routes and configurate to run API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import express from 'express';
 import pino from 'pino';
+import dotenv from 'dotenv';
+import router from './presentation/routes/api.routes.js';
+
+dotenv.config();
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 3000;
 
 const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
@@ -16,5 +22,25 @@ const logger = pino({
   }),
 });
 
-// Start the application
-logger.info('Application started');
+const app = express();
+
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`);
+  logger.debug('Headers:', req.headers);
+  next();
+});
+
+app.use(express.json());
+app.use('/api', router);
+
+app.use((err, res) => {
+  logger.error(err);
+  res.status(err.status || 500).json({
+    status: 'error',
+    message: err.message || 'Internal Server Error',
+  });
+});
+
+app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
+});
